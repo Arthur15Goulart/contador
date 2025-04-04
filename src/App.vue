403bdb0bdd9338480385aca3433923d3e454052d
Arthur15Goulart <arthurgoulartteixeirafabio48@gmail.com>
2025-04-04 15:03:42 -0300

    nova-conta

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,69 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+    const nome = 'Arthur'
+    const idade = 16
 
-const contador = ref(0)
+    function inverter(nome) {
+        return nome.toUpperCase().split('').reverse().join('')
+    }
 
-function incrementarContador() {
-  contador.value++
+    function saudacao() {
+        return `Olá, ${nome}!`
+    }
+    import { ref } from 'vue';
+
+const contador = ref(0);
+
+
+function incrementar() {
+  contador.value++;
 }
-function decrementarContador() {
-  contador.value--
+
+function decrementar() {
+  if (contador.value > 0) {
+    contador.value--;
+  }
 }
 
-onMounted(() => {
-  console.log(`O valor inicial do contador é ${contador.value}.`)
-})
+function reiniciar() {
+  contador.value = 0;
+}
 </script>
 
 <template>
+    <div class="info">
+        <h1>Exemplo 1 - Mostrando informações na tela</h1>
+        <p>Nome: {{ nome }} </p>
+        <p>Idade: {{ idade }}  anos</p>
+        <hr />
+        <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
+        <p>O nome tem {{ nome.length }}  caracteres</p>
+        <p>O nome invertido é: {{ inverter(nome) }} </p>
+        <hr />
+        <p> Exemplo de saudação usando função</p>
+        <p> {{ saudacao() }}  </p>
+    </div>
 
-<button @click="incrementarContador">Incrementar</button>
-  <button @click="decrementarContador">Decrementar</button>
-  <p>Valor do contador é: {{ contador }}</p>
-  
+    <div class="info">
+    <h1>Contador</h1>
+    <p> {{ contador }} </p>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
+    <button @click="reiniciar">Reiniciar</button>
+  </div>
 </template>
 
 <style scoped>
-button {
+.info {
+  background-color: rgba(35,12,22,1);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
+  margin : 65px 150px;
+}
+
+h1 {
+  color: white;
   font-weight: bold;
+  font-size: 1.5em;
 }
 </style>
